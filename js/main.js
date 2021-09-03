68170c52ba55e1dbe8c11c38b7c7dc9d2102c756
Fur1ouS <furiticus@gmail.com>
2021-09-03 17:29:09 +0300

    add all style

---
+++ js/main.js
@@ -0,0 +1,47 @@
+new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar'
+    },
+    autoHeight: true,
+    slidesPerView: 3,
+    loop: true,
+    freeMode: true,
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    }
+});
+
+$(document).ready(function () {
+    $(".head-order").on("click", "a", function (event) {
+        event.preventDefault();
+        let id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+// timer
+const countDownEl = document.getElementById('timer');
+let time = 1800;
+
+setInterval(function (){
+    let min = Math.floor(time / 60);
+    let sec = time % 60;
+
+    if(min === '00'){
+        min = Math.floor(time / 60);
+    }
+
+    min = min < 10 ? '0'+min : min;
+    sec = sec < 10 ? '0'+sec : sec;
+
+    countDownEl.innerHTML = `00 : ${min} : ${sec}`;
+    time--;
+}, 1000);
